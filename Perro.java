59c7a798388e6d1ac3923aae41aaa721092e50f1
madeleinnehenao <124703743+madeleinnehenao@users.noreply.github.com>
2023-05-31 22:13:28 -0500

    Add files via upload

---
+++ Perro.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package animales;
+
+public class Perro extends Canido {   
+   
+    public String getSonido(){
+        return "Ladrido";
+    }
+    public String getAlimentos() {
+        return "Carnivora";
+    }
+    public String getHabitat() {
+        return "Domestico";
+    }
+    public String getNombreCientifico() {
+        return "Canis lupus familiaris";
+    }
+    
+}
